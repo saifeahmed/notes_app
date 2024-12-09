395585e02f8ae942e0e737469038b85fceef21bc
saifeahmed <ahmedsaife24@gmail.com>
2024-12-10 01:30:15 +0200

    made list of notes

---
+++ lib/widgets/custom_note.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomNote extends StatelessWidget {
+  const CustomNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 12),
+      decoration: BoxDecoration(
+        color: Colors.amber[300],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Note name",
+                style: TextStyle(color: Colors.black, fontSize: 32),
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Build your career with saife",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 20),
+              ),
+            ),
+            trailing: Icon(
+              size: 35,
+              Icons.delete_rounded,
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 25, bottom: 25),
+            child: Text(
+              "may 12,2022",
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/custome_view_body.dart
+++ lib/widgets/custome_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_note.dart';
 import 'package:notes_app/widgets/custome_app_par.dart';
 
 class CustomeViewBody extends StatelessWidget {
@@ -9,8 +10,31 @@ class CustomeViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(26),
       child: Column(
-        children: [CustomAppBar()],
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: CustomAppBar(),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: ListNotes())
+        ],
       ),
     );
+  }
+}
+
+class ListNotes extends StatelessWidget {
+  const ListNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: CustomNote(),
+      );
+    });
   }
 }
